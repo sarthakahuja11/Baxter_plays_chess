0d9f5df06888b4c11eab19e268ea455f1282422d
Sarthak Ahuja <sarthakahuja11@users.noreply.github.com>
2022-03-13 22:35:17 +0000

    updated with linda changes

--- chess_baxter/src/pick_and_place_moveit.py
+++ chess_baxter/src/pick_and_place_moveit.py
@@ -22,6 +22,8 @@ from gazebo_msgs.srv import (
 
 import baxter_interface
 import moveit_commander
+import spawn_chessboard
+import delete_chessgame
 
 
 class PickAndPlaceMoveIt(object):
@@ -120,47 +122,6 @@ class PickAndPlaceMoveIt(object):
         self._retract()
 
 
-def load_gazebo_models(table_pose=Pose(position=Point(x=1.0, y=0.0, z=0.0)),
-                       table_reference_frame="world",
-                       block_pose=Pose(position=Point(x=0.68, y=0.11, z=0.7825)),
-                       block_reference_frame="world"):
-    # Get Models' Path
-    model_path = rospkg.RosPack().get_path('baxter_sim_examples')+"/models/"
-    # Load Table SDF
-    table_xml = ''
-    with open(model_path + "cafe_table/model.sdf", "r") as table_file:
-        table_xml = table_file.read().replace('\n', '')
-    # Load block SDF
-    block_xml = ''
-    with open(model_path + "block/model.sdf", "r") as block_file:
-        block_xml = block_file.read().replace('\n', '')
-    # Spawn Table SDF
-    rospy.wait_for_service('/gazebo/spawn_sdf_model')
-    try:
-        spawn_sdf = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
-        spawn_sdf("cafe_table", table_xml, "/", table_pose, table_reference_frame)
-    except rospy.ServiceException, e:
-        rospy.logerr("Spawn SDF service call failed: {0}".format(e))
-    # Spawn block SDF
-    rospy.wait_for_service('/gazebo/spawn_sdf_model')
-    try:
-        spawn_sdf = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
-        spawn_sdf("block", block_xml, "/", block_pose, block_reference_frame)
-    except rospy.ServiceException, e:
-        rospy.logerr("Spawn SDF service call failed: {0}".format(e))
-
-
-def delete_gazebo_models():
-    # This will be called on ROS Exit, deleting Gazebo models
-    # Do not wait for the Gazebo Delete Model service, since
-    # Gazebo should already be running. If the service is not
-    # available since Gazebo has been killed, it is fine to error out
-    try:
-        delete_model = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)
-        delete_model("cafe_table")
-        delete_model("block")
-    except rospy.ServiceException, e:
-        rospy.loginfo("Delete Model service call failed: {0}".format(e))
 
 
 def main():
@@ -170,9 +131,9 @@ def main():
     # Load Gazebo Models via Spawning Services
     # Note that the models reference is the /world frame
     # and the IK operates with respect to the /base frame
-    #load_gazebo_models()
+    # load_gazebo_models()
     # Remove models from the scene on shutdown
-    rospy.on_shutdown(delete_gazebo_models)
+    # rospy.on_shutdown(delete_gazebo_models)
 
     # Wait for the All Clear from emulator startup
     rospy.wait_for_message("/robot/sim/started", Empty)
@@ -211,8 +172,8 @@ def main():
     
     piece_positions = rospy.get_param('piece_target_position_map')
     
-    pick_positions = ['00']#, '07', '07', '02', '76']
-    place_positions = ['44']#, '34', '23', '44', '55']
+    pick_positions = ['13', '63', '14', '64', '01']
+    place_positions = ['33', '43', '24', '54', '22']
     
     pick_position_poses = []
     place_position_poses = []
@@ -227,11 +188,11 @@ def main():
     	    
     
     idx = 0
-    while not rospy.is_shutdown():
-    	for i in range(1):
-        	print("\nPicking...")
+    if not rospy.is_shutdown():
+    	for i in range(5):
+        	print("\nPicking "+ pick_positions[i]+"...")
         	pnp.pick(pick_position_poses[i])
-        	print("\nPlacing...")
+        	print("\nPlacing "+ place_positions[i]+"...")
         	pnp.place(place_position_poses[i])
     return 0
 
